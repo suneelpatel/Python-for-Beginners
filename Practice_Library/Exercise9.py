5a510f82fd9ef2c88e2e42d639995968e468dede
Suneel Patel <48252015+suneelpatel@users.noreply.github.com>
2019-07-27 18:40:43 +0530

    Update Exercise9.py

--- Practice_Library/Exercise9.py
+++ Practice_Library/Exercise9.py
@@ -1,3 +1,13 @@
+# Case:Please write a program which accepts a string from console and print it in reverse order. 
+
+# Example: If the following string is given as input to the program: 
+# rise to vote sir 
+
+# Then, the output of the program should be: 
+# ris etov ot esir
+
+
+
 str=input("Enter string which you want to reverse: ")
 
 print("Reverse of the string is: ")
