2eaec5247818c2084327b37eddeab0a46125c6f5
Suneel Patel <48252015+suneelpatel@users.noreply.github.com>
2019-07-27 18:25:05 +0530

    Update Exercise3.py

--- Practice_Library/Exercise3.py
+++ Practice_Library/Exercise3.py
@@ -1,3 +1,5 @@
+# Case: Write a program, which will find all the numbers between 1000 and 3000 (both included) such that each digit of a number is an even number. The numbers obtained should be printed in a comma separated sequence on a single line.
+# Hint: In case of input data being supplied to the question, it should be assumed to be a console input. Divide each digit with 2 and verify is it even or not.
 
 values=[]
 
