e857825ab26940787b420b34c311e8d071fac7f3
Suneel Patel <48252015+suneelpatel@users.noreply.github.com>
2019-07-27 18:20:19 +0530

    Update Exercise1.py

--- Practice_Library/Exercise1.py
+++ Practice_Library/Exercise1.py
@@ -1,3 +1,6 @@
+# Case1: Write a program which will find factors of given number and find whether the factor is even or odd.
+# Hint: Use Loop with if-else statements
+
 num=int(input("Enter the number:"))
 
 for i in range(1,num+1):
