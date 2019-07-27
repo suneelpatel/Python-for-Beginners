92fe687737bc0a6b16a938e690d8d7ffa6957342
Suneel Patel <48252015+suneelpatel@users.noreply.github.com>
2019-07-27 18:36:21 +0530

    Update Exercise6.py

--- Practice_Library/Exercise6.py
+++ Practice_Library/Exercise6.py
@@ -1,7 +1,16 @@
-# What is the output of the following code? 
-# nums = set([1,1,2,3,3,3,4,4]) 
-# print(len(nums))
-# Hint: Set consists unique element.
+#Case 7:  Please write a program which count and print the numbers of each character in a string input by console. 
+
+# Example: If the following string is given as input to the program: 
+# abcdefgabc 
+# Then, the output of the program should be: 
+# a,2 
+# c,2 
+# b,2 
+# e,1 
+# d,1 
+# g,1
+# f,1
+
 
 dic = {}
 
