1d1a680eb67fc0d7b13049d6484d48dc244e0179
Suneel Patel <48252015+suneelpatel@users.noreply.github.com>
2019-07-27 18:34:55 +0530

    Update Exercise7.py

--- Practice_Library/Exercise7.py
+++ Practice_Library/Exercise7.py
@@ -1,3 +1,6 @@
+# Case 9: With a given list [12,24,35,24,88,120,155,88,120,155], write a program to print this list after removing all duplicate values with original order reserved. 
+
+#Solution:
 
 li = [12, 24, 35, 24, 88, 120, 155, 88, 120, 155]
 def RemoveDuplicate(li):
