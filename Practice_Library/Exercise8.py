890027fc9195ea6a7e07bf00b2f7468c3574e2a5
Suneel Patel <48252015+suneelpatel@users.noreply.github.com>
2019-07-27 18:39:04 +0530

    Update Exercise8.py

--- Practice_Library/Exercise8.py
+++ Practice_Library/Exercise8.py
@@ -1,3 +1,8 @@
+# Case: By using list comprehension, please write a program to print the list after removing the 0th,4th,5th numbers in [12,24,35,70,88,120,155]. 
+
+# Solution:
+
+
 li=[12,24,35,70,88,120,155]
 li = [x for (i,x) in enumerate(li) if i not in (0,4,5)]
 print(li)
