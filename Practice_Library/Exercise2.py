27a38f6236720f36a260afcb90a93497b1a6808e
Suneel Patel <48252015+suneelpatel@users.noreply.github.com>
2019-07-27 18:22:31 +0530

    Update Exercise2.py

--- Practice_Library/Exercise2.py
+++ Practice_Library/Exercise2.py
@@ -1,3 +1,6 @@
+# Write a code which accepts a sequence of words as input and prints the words in a sequence after sorting them alphabetically.
+# Hint: In case of input data being supplied to the question, it should be assumed to be a console input.
+
 my_str=input("Enter your word: ")
 
 words=my_str.split()
