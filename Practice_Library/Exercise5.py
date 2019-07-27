28581f2c128e99f188bc94d3ba3122841c690f68
Suneel Patel <48252015+suneelpatel@users.noreply.github.com>
2019-07-27 18:27:30 +0530

    Update Exercise5.py

--- Practice_Library/Exercise5.py
+++ Practice_Library/Exercise5.py
@@ -1,3 +1,6 @@
+# Case: Design a code which will find the given number is Palindrome number or not.
+# Hint: Use built-in functions of string.
+
 my_str=input("Enter the Value: ")  # change this value for a different output
 
 my_str=my_str.casefold() # make it suitable for caseless comparison
