33b8a5cbf3817547a90cea759aacfd5b33fcadde
Suneel Patel <48252015+suneelpatel@users.noreply.github.com>
2019-07-28 19:20:49 +0530

    Add files via upload

--- Practice_Library/Exercise10.py
+++ Practice_Library/Exercise10.py
@@ -1,5 +1,2 @@
-# Case: Please write a program to randomly generate a list with 5 numbers, which are divisible by 5 and 7 , between 1 and 1000 inclusive. 
-
-
 import random
-print(random.sample([i for i in range(1,1001) if i%5==0 and i%7==0],5))
+print(random.sample([i for i in range(1,1001) if i%5==0and i%7==0],5))
