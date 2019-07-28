860b48e916ed1a7988e0efe66f6e1dbf5651992d
Suneel Patel <48252015+suneelpatel@users.noreply.github.com>
2019-07-28 19:28:19 +0530

    Update Exercise12.py

--- Practice_Library/Exercise12.py
+++ Practice_Library/Exercise12.py
@@ -1,3 +1,9 @@
+# Case: Write a for loop that prints all elements of a list and their position in the list. 
+# a = [4,7,3,2,5,9] 
+
+# Hint: Use Loop to iterate through list elements.
+
+
 a = [4,7,3,2,5,9]
 
 for item in a:
