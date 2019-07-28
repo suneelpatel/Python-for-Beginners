c76f8e658e3d2b5dc309abfccf2aeb343a993c2e
Suneel Patel <48252015+suneelpatel@users.noreply.github.com>
2019-07-28 19:33:40 +0530

    Update Exercise15.py

--- Practice_Library/Exercise15.py
+++ Practice_Library/Exercise15.py
@@ -1,3 +1,6 @@
+# Case : With two given lists [1,3,6,78,35,55] and [12,24,35,24,88,120,155], 
+# write a program to make a list whose elements are intersection of the above given lists.
+
 set1=set([1,3,6,78,35,55])
 set2=set([12,24,35,24,88,120,155])
 
