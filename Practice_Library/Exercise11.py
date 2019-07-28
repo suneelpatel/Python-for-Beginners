5cc920e46c1ad7eccc40ad2e673cda8a0793a78d
Suneel Patel <48252015+suneelpatel@users.noreply.github.com>
2019-07-28 19:26:14 +0530

    Update Exercise11.py

--- Practice_Library/Exercise11.py
+++ Practice_Library/Exercise11.py
@@ -1,3 +1,10 @@
+# Case: Please write a program which accepts a string from console and print the characters that have even indexes. 
+# Example: If the following string is given as input to the program: 
+# H1e2l3l4o5w6o7r8l9d 
+# Then, the output of the program should be: 
+# Helloworld
+
+
 s=input("Enter string:")
 s = s[::2]
 print(s)
