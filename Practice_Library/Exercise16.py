107e64bc10ea70c31e120f12397b73b11dfc3e3f
Suneel Patel <48252015+suneelpatel@users.noreply.github.com>
2019-07-28 19:34:51 +0530

    Update Exercise16.py

--- Practice_Library/Exercise16.py
+++ Practice_Library/Exercise16.py
@@ -1,3 +1,17 @@
+"""
+Case: A website requires a user to input username and password to register. Write a program to check the validity of password given by user. Following are the criteria for checking password: 
+1. At least 1 letter between [a-z] 
+2. At least 1 number between [0-9] 
+3. At least 1 letter between [A-Z] 
+4. At least 1 character from [$#@] 
+5. Minimum length of transaction password: 6 
+6. Maximum length of transaction password: 12 
+
+Hint: In case of input data being supplied to the question, it should be assumed to be a console input.
+
+"""
+
+
 print("Enter Password")
 
 print("The password must be at least 6, and no more than 12 characters long")
