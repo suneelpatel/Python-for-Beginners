7cf819103acbcb286e636d1320b79e2e2727971b
Suneel Patel <48252015+suneelpatel@users.noreply.github.com>
2019-07-28 19:30:26 +0530

    Update Exercise13.py

--- Practice_Library/Exercise13.py
+++ Practice_Library/Exercise13.py
@@ -1,3 +1,13 @@
+# Case: Write a program to compute 1/2+2/3+3/4+...+n/n+1 with a given n input by console (n>0) 
+
+# Example: 
+# If the following n is given as input to the program: 
+# 5 
+# Then, the output of the program should be: 
+# 3.55
+
+
+
 n=int(input("Enter a number: "))
 
 sum=0.0
